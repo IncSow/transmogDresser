465f0aa5f5fb1750cc675d79762c99a50de4ed84
IncSow <lilo.incsow@gmail.com>
2024-05-15 14:45:48 +0200

    Added a few gems

--- test/controllers/body_parts_controller_test.rb
+++ test/controllers/body_parts_controller_test.rb
@@ -1,45 +1,47 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class BodyPartsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @body_part = body_parts(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get body_parts_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_body_part_url
     assert_response :success
   end
 
-  test "should create body_part" do
-    assert_difference("BodyPart.count") do
+  test 'should create body_part' do
+    assert_difference('BodyPart.count') do
       post body_parts_url, params: { body_part: { name: @body_part.name } }
     end
 
     assert_redirected_to body_part_url(BodyPart.last)
   end
 
-  test "should show body_part" do
+  test 'should show body_part' do
     get body_part_url(@body_part)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_body_part_url(@body_part)
     assert_response :success
   end
 
-  test "should update body_part" do
+  test 'should update body_part' do
     patch body_part_url(@body_part), params: { body_part: { name: @body_part.name } }
     assert_redirected_to body_part_url(@body_part)
   end
 
-  test "should destroy body_part" do
-    assert_difference("BodyPart.count", -1) do
+  test 'should destroy body_part' do
+    assert_difference('BodyPart.count', -1) do
       delete body_part_url(@body_part)
     end
 
--- test/system/outfits_test.rb
+++ test/system/outfits_test.rb
@@ -1,45 +1,47 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class OutfitsTest < ApplicationSystemTestCase
   setup do
     @outfit = outfits(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit outfits_url
-    assert_selector "h1", text: "Outfits"
+    assert_selector 'h1', text: 'Outfits'
   end
 
-  test "should create outfit" do
+  test 'should create outfit' do
     visit outfits_url
-    click_on "New outfit"
+    click_on 'New outfit'
 
-    fill_in "Name", with: @outfit.name
-    fill_in "User", with: @outfit.user_id
-    check "Visibility" if @outfit.visibility
-    click_on "Create Outfit"
+    fill_in 'Name', with: @outfit.name
+    fill_in 'User', with: @outfit.user_id
+    check 'Visibility' if @outfit.visibility
+    click_on 'Create Outfit'
 
-    assert_text "Outfit was successfully created"
-    click_on "Back"
+    assert_text 'Outfit was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Outfit" do
+  test 'should update Outfit' do
     visit outfit_url(@outfit)
-    click_on "Edit this outfit", match: :first
+    click_on 'Edit this outfit', match: :first
 
-    fill_in "Name", with: @outfit.name
-    fill_in "User", with: @outfit.user_id
-    check "Visibility" if @outfit.visibility
-    click_on "Update Outfit"
+    fill_in 'Name', with: @outfit.name
+    fill_in 'User', with: @outfit.user_id
+    check 'Visibility' if @outfit.visibility
+    click_on 'Update Outfit'
 
-    assert_text "Outfit was successfully updated"
-    click_on "Back"
+    assert_text 'Outfit was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Outfit" do
+  test 'should destroy Outfit' do
     visit outfit_url(@outfit)
-    click_on "Destroy this outfit", match: :first
+    click_on 'Destroy this outfit', match: :first
 
-    assert_text "Outfit was successfully destroyed"
+    assert_text 'Outfit was successfully destroyed'
   end
 end
